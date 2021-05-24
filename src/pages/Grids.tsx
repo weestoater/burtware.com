67f8c6dd91e3cbfa7d3ecab30a622f630fc983c5
Ian <ian@weestoater.com>
2021-05-25 00:16:25 +0100

    style changes

--- src/pages/Grids.tsx
+++ src/pages/Grids.tsx
@@ -4,9 +4,6 @@ import MatchesGrid from "../components/grids/MatchesGrid";
 export default function Grids() {
   return (
     <div className="row">
-      <div className="col-sm-12">
-        <h1>Grids</h1>
-      </div>
       <MatchesGrid />
     </div>
   );
