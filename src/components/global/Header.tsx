48a8579371fcf30faa8e75199ce468ea7c007f09
Ian Burrett <ian@weestoater.com>
2022-04-15 17:54:00 +0100

    a11y changes; home page content

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -7,7 +7,7 @@ interface HeaderProps {
 export default function Header(props: HeaderProps) {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <i className="bi bi-code-slash text-warning mx-4" aria-label="code" />
+      <i className="bi bi-code-slash text-warning mx-1" aria-label="code" />
       <NavLink className="navbar-brand" to="/">
         {props.site}
       </NavLink>
---
+++ src/components/home/AwsAmplifyPost.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function AwsAmplifyPost() {
+  return (
+    <div className="col-lg-3 col-md-6 col-sm-12">
+      <div className="card mb-3">
+        <div className="card-header">
+          <i className="bi bi-cloud" />
+          <span className="pl-4">AWS Amplify</span>
+        </div>
+        <div className="card-body">
+          <p>
+            I wanted to get an AWS instance up and running, as quickly as possible. So I have used 
+            AWS Amplify to power this project.  At the moment, I have it set to build and deploy from 
+            my github instance and apart from some 'minor' issues in the beginning, it has been fairly
+            pain free.
+          </p>
+          <p>
+            There are some great tutorials on setting up testing as part of the deploy process 
+            so that will probably be where I begin to focus my time in the near future.</p>
+            
+            <p>Right now 
+            I just want to add some more 'content' and get it working smoothly as a process.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
