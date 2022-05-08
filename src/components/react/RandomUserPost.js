16d92983390b0f44a8689aa98544d17b10b6f56d
Ian Burrett <ian@weestoater.com>
2022-05-08 20:41:30 +0100

    Adding randomUser api example

---
+++ src/components/react/RandomUserPost.js
@@ -0,0 +1,32 @@
+import React, {useState, useEffect} from 'react';
+import getApi from '../../adapters/commonApi';
+import Spinner from '../global/Spinner';
+import UserItem from './UserItem';
+
+export default function RandomUsersPost() {
+
+    const [users, setUsers] = useState({});
+    const apiUrl = "https://randomuser.me/api/?results=3";
+
+    useEffect(() => {
+        getApi(apiUrl).then(setUsers);
+      }, []);
+
+    return (
+        <div className="col-lg-3 col-md-6 mb-4">
+            <div className="card">
+                <div className="card-header">
+                    <i className="bi bi-filetype-jsx" />
+                    <span className="pl-4">Random Users API</span>
+                </div>
+                <div className="card-body users">
+                {users.results ? (
+                    users.results.map((user, i) => <UserItem key={i} item={user} /> )
+                ) : ( 
+                <Spinner /> 
+                )}
+                </div>
+            </div>
+        </div>
+    );
+}
