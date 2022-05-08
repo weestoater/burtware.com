02ef56b46d5fc6c085a754cde9b386f8a81f33de
Ian Burrett <ian@weestoater.com>
2022-05-08 20:47:55 +0100

    Updated spinner

--- src/components/global/Spinner.tsx
+++ src/components/global/Spinner.tsx
@@ -3,8 +3,10 @@ import React from 'react';
 export default function Spinner() {
     return (
         <div className="row">
-            <div className="col-lg-2 offset-lg-5 text-center m-4">
-                <i className="fa fa-circle-o-notch fa-spin fa-2x text-success" />
+            <div className="d-flex justify-content-center">
+                <div className="spinner-border" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </div>
             </div>
         </div>
     );
