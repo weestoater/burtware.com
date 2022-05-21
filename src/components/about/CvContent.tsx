578d2e8c77d3990913b8700500d57990f8463ca9
Ian Burrett <ian@weestoater.com>
2022-05-21 14:59:32 +0100

    Adding burtware CV

---
+++ src/components/about/CvContent.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import cvData from "../../data/burtware-cv.json";
+
+
+export default function CvContent() {
+    const jobs = cvData.length;
+
+    const jobsList = cvData.map((job, key) => 
+        <li key={key}>
+            <div className="date">{job.startDate} <small className="bi bi-chevron-right text-info"></small> {job.endDate}</div>
+            <div className="role">{job.jobTitle}</div>
+            <div className="company">{job.company}</div>
+        </li>
+    );
+
+    return (
+
+        <div className="col-lg-3 col-md-6">
+            <div className="card mb-4">
+                <div className="card-header">
+                <i className="bi bi-balloon-heart" /> burtware CV ({ jobs })
+                </div>
+                <div className="card-body">
+                    <div className="cvtimeline">
+                        <ul>
+                            {jobsList}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import AboutContent from "../components/about/AboutContent";
+import CvContent from "../components/about/CvContent";
 
 export default function About() {
   return (
     <div className="row">
       <AboutContent />
+      <CvContent />
     </div>
   );
 }
