53fa90587f00e123d44cbfe245abce0b05c5c725
Ian Burrett <ian@weestoater.com>
2022-06-09 22:44:11 +0100

    changing theme to camo

--- src/components/football/intro.tsx
+++ src/components/football/intro.tsx
@@ -5,7 +5,11 @@ export default function Intro() {
     return (
         <div className="row">
             <div className="col-sm-12">
-                <h1><img src={mfclogo} className="mfc-logo" alt="" /> Motherwell F.C.</h1>
+                <div className="card">
+                    <div className="card-header">
+                        <h1><img src={mfclogo} className="mfc-logo" alt="" /> Motherwell F.C.</h1>
+                    </div>
+                </div>
             </div>
         </div>
     );
