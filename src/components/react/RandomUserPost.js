3da36fae16f5b704db0bb8773b84a38d483c25f1
Ian Burrett <ian@weestoater.com>
2022-06-09 22:12:55 +0100

    responsive tweaks

--- src/components/react/RandomUserPost.js
+++ src/components/react/RandomUserPost.js
@@ -13,7 +13,7 @@ export default function RandomUsersPost() {
       }, []);
 
     return (
-        <div className="col-lg-3 col-md-6 mb-4">
+        <div className="col-lg-4 col-md-6 mb-4">
             <div className="card">
                 <div className="card-header">
                     <i className="bi bi-filetype-jsx" />
