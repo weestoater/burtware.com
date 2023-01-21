cdc9e6416ed035b68747406a50fc974283c0628e
Ian Burrett <ian@weestoater.com>
2023-01-21 13:04:44 +0000

    typo fix

--- src/components/a11y/A11ySME.tsx
+++ src/components/a11y/A11ySME.tsx
@@ -10,7 +10,7 @@ export default function A11ySME() {
                 <div className="card-body">
                     <p>I am incredibly lucky to work for a company who support and encourage my passion for A11y.  I was promoted to Vice President for the work I've been doing in this area and I am now even <b>more</b> enthusiastic to prove how important it is and spread my net wider in the firm.</p>
 
-                    <p>I love engaging with teams and helping them discover the range of tools available and the steps they need to take to reach and keep A11y compliance. Often there are one of two developers who rise to the challenge and drive their team. This is the best result for everyone - as they can then carry the torch and keep the a11y issues at a minimum.</p>
+                    <p>I love engaging with teams and helping them discover the range of tools available and the steps they need to take to reach and keep A11y compliance. Often there are one or two developers who rise to the challenge and drive their team. This is the best result for everyone - as they can then carry the torch and keep a11y issues to a minimum.</p>
 
                     <p>I was recently told by a fresh University graduate that A11y isn't even mentioned during a <b>4 year</b> Computer Science degree - I am absolutely shocked to hear this as legislation has been in place for over 20+ years. With this in mind I am looking to become more involved in our Communities of Practise and POWERUP events internally to spread the good word and more importantly, offer education and advice to young or inexperienced developers.</p>
                 </div>
