c66f7e5e9f98e4cb9f3a5459671432a5e42e4933
Ian Burrett <ian@weestoater.com>
2023-02-12 13:02:10 +0000

    remove console log

--- src/components/football/shownotes.tsx
+++ src/components/football/shownotes.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 
 export const ShowNotes = (props: any) => {
     const match = props;
-    console.log('match -> ' + JSON.stringify(match));
     if (match.notes == null || match.notes == undefined) {
         return null;
     } else {
