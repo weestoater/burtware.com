297e64e193ca7a35876f9b53e4aa43b4d15926f5
Ian Burrett <ian@weestoater.com>
2023-05-14 12:51:39 +0100

    St.Johnstone game

--- src/components/football/goals.tsx
+++ src/components/football/goals.tsx
@@ -3,7 +3,7 @@ import goalsData  from "../../data/mfc-goals.json";
 
 export default function Goals() {
     const seasonslist = goalsData.map((item, key) => 
-        <div className="col-lg-4 col-md-4 col-sm-12 mb-2" key={key}>
+        <div className="col mb-2" key={key}>
             <h3>{item.season}</h3>
             <ul className="list-group goals-list">
                 {item.details.map((gs, index) =>
@@ -15,10 +15,9 @@ export default function Goals() {
     );
 
     return (
-        <div className="col-lg-6 col-md-12 mb-4">
+        <div className="col mb-4">
             <div className="row">
-                <h2 className="text-black">&#9917; Goal scorers</h2>
-                <hr />
+                <h2 className="text-black">&#9917; Goals</h2>
                 {seasonslist}
             </div>
         </div>
